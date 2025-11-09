09e44eae721473310e5db702c089dd3ab06be843
mazharenko <mazharenko.a@gmail.com>
2025-11-09 18:33:33 +0000

    update year

--- tests/AoC.Tests/InputTests.cs
+++ tests/AoC.Tests/InputTests.cs
@@ -3,7 +3,7 @@ using mazharenko.AoCAgent.Generator;
 namespace AoC.Tests;
 
 [TestFixture]
-[GenerateInputTests(2024, nameof(GetCases))]
+[GenerateInputTests(2025, nameof(GetCases))]
 internal partial class InputTests
 {
     private static IEnumerable<PartInputCaseData> GetCases()
